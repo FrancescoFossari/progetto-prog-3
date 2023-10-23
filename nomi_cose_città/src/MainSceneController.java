00a6ab710b10b8758f34533f7ea121ceb0a978c3
MARCOP001 <pisacanemarco01@gmail.com>
2023-10-23 17:51:48 +0200

    MainSceneController
    dfdf

---
+++ nomi_cose_città/src/MainSceneController.java
@@ -0,0 +1,24 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class MainSceneController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void btnClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
