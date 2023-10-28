925ce39798bff2c881a6ee452f89e447510ae34f
ClaudioMarotta05 <117671050+ClaudioMarotta05@users.noreply.github.com>
2023-10-28 08:54:58 +0200

    aggiornamento

--- nomi_cose_città/src/Amministratore.java
+++ nomi_cose_città/src/Amministratore.java
@@ -4,11 +4,11 @@ import java.util.Random;
 
 public class Amministratore {
     private static final int NUMERO_CATEGORIE_DA_SCEGLIERE = 5;
-    private List<String> categorieDisponibili;
+    private List<String> categorieScelte;
     
     public Amministratore() {
         // Inizializza tutte le categorie disponibili
-        categorieDisponibili = new ArrayList<>();
+        List<String> categorieDisponibili = new ArrayList<>();
         categorieDisponibili.add("Nomi di persona");
         categorieDisponibili.add("Cose (oggetti)");
         categorieDisponibili.add("Città");
@@ -20,11 +20,13 @@ public class Amministratore {
         categorieDisponibili.add("Nazioni");
         categorieDisponibili.add("Mestieri");
         categorieDisponibili.add("Personaggi famosi o storici");
+        
+        categorieScelte = scegliCategorie(categorieDisponibili, NUMERO_CATEGORIE_DA_SCEGLIERE);
     }
 
-    // Scegli casualmente cinque categorie
-    public List<String> scegliCategorie() {
-        if (categorieDisponibili.size() < NUMERO_CATEGORIE_DA_SCEGLIERE) {
+    // Scegli casualmente le categorie da un elenco
+    private List<String> scegliCategorie(List<String> categorieDisponibili, int numeroCategorie) {
+        if (categorieDisponibili.size() < numeroCategorie) {
             System.out.println("Non ci sono abbastanza categorie disponibili.");
             return null;
         }
@@ -32,7 +34,7 @@ public class Amministratore {
         Random random = new Random();
         List<String> categorieScelte = new ArrayList<>();
         
-        for (int i = 0; i < NUMERO_CATEGORIE_DA_SCEGLIERE; i++) {
+        for (int i = 0; i < numeroCategorie; i++) {
             int indiceCasuale = random.nextInt(categorieDisponibili.size());
             String categoriaScelta = categorieDisponibili.get(indiceCasuale);
             categorieScelte.add(categoriaScelta);
@@ -42,9 +44,14 @@ public class Amministratore {
         return categorieScelte;
     }
 
+    // Restituisce le categorie scelte dall'amministratore
+    public List<String> getCategorieScelte() {
+        return categorieScelte;
+    }
+
     public void avviaGioco(int numGiocatori) {
         List<Thread> giocatoriThreads = new ArrayList<>();
-        List<String> categorieGiocatore = scegliCategorie();
+        List<String> categorieGiocatore = categorieScelte;
 
         if (categorieGiocatore == null) {
             System.out.println("Impossibile avviare il gioco senza categorie sufficienti.");
@@ -54,7 +61,7 @@ public class Amministratore {
         for (int i = 1; i <= numGiocatori; i++) {
             String nomeGiocatore = "Giocatore " + i;
             
-            Runnable giocatore = new Giocatore(nomeGiocatore, categorieGiocatore);
+            Runnable giocatore = new Giocatore(nomeGiocatore, categorieGiocatore, this);
             Thread threadGiocatore = new Thread(giocatore);
             giocatoriThreads.add(threadGiocatore);
             threadGiocatore.start();
@@ -71,6 +78,13 @@ public class Amministratore {
 
     public static void main(String[] args) {   
         Amministratore amministratore = new Amministratore();
+
+        // Passa le categorie scelte a una funzione griglia o inviale ai client
+        System.out.println("Categorie scelte dall'amministratore:");
+        for (String categoria : amministratore.getCategorieScelte()) {
+            System.out.println(categoria);
+        }
+
         amministratore.avviaGioco(3); // Specifica il numero di giocatori
     }
 }
@@ -78,15 +92,17 @@ public class Amministratore {
 class Giocatore implements Runnable {
     private String nome;
     private List<String> categorie;
+    private Amministratore amministratore;
 
-    public Giocatore(String nome, List<String> categorie) {
+    public Giocatore(String nome, List<String> categorie, Amministratore amministratore) {
         this.nome = nome;
         this.categorie = categorie;
+        this.amministratore = amministratore;
     }
 
     @Override
     public void run() {
-        // Implementa il comportamento del giocatore durante il gioco
+        // Utilizza le categorie scelte dall'amministratore
         for (String categoria : categorie) {
             // Puoi fare quello che vuoi con la categoria scelta
             System.out.println(nome + " ha scelto la categoria: " + categoria);
