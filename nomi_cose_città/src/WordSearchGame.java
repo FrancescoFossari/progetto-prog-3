9307204618f6a680757f682a86e2235d85e360e1
Francesco Fossari <74236742+FrancescoFossari@users.noreply.github.com>
2023-11-15 12:15:17 +0100

    modifica_dizionario
    aaaaa

--- nomi_cose_città/src/WordSearchGame.java
+++ nomi_cose_città/src/WordSearchGame.java
@@ -1,36 +1,76 @@
-
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
+
+// Interfaccia per la strategia di ricerca
+interface SearchStrategy {
+    String search(char startingLetter, String filePath);
+}
+
+// Implementazione della strategia di ricerca casuale nei file
+class RandomFileSearchStrategy implements SearchStrategy {
+    @Override
+    public String search(char startingLetter, String filePath) {
+        List<String> matchingWords = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                line = line.trim(); // Rimuovi spazi
+                if (!line.isEmpty() && Character.toLowerCase(line.charAt(0)) == Character.toLowerCase(startingLetter)) {
+                    matchingWords.add(line);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (!matchingWords.isEmpty()) {
+            // Se ci sono parole corrispondenti, restituisci una casuale
+            Random random = new Random();
+            return matchingWords.get(random.nextInt(matchingWords.size()));
+        } else {
+            return null; // Nessuna parola trovata nel file
+        }
+    }
+}
 
 public class WordSearchGame {
     public static void main(String[] args) {
-        // Elenco di file da cercare
-        List<String> files = new ArrayList<>();
-        files.add("nomi_cose_città/src/dizionario/nomi.txt");
-        files.add("nomi_cose_città/src/dizionario/cose.txt");
-        files.add("nomi_cose_città/src/dizionario/citta.txt");
-        files.add("nomi_cose_città/src/dizionario/cantanti.txt");
-        files.add("nomi_cose_città/src/dizionario/mestieri.txt");
-        files.add("nomi_cose_città/src/dizionario/nomi.txt");
-        files.add("nomi_cose_città/src/dizionario/frutta.txt");
-        files.add("nomi_cose_città/src/dizionario/animali.txt");
-        
-        // Crea strategia di ricerca sequenziale
-        SearchStrategy fileSearchStrategy = new SequentialFileSearchStrategy();
+        // Crea strategia di ricerca casuale
+        SearchStrategy fileSearchStrategy = new RandomFileSearchStrategy();
 
-        // Richiedi la parola da cercare all'utente
+        // Richiedi la lettera e il file da cercare all'utente
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Inserisci la parola da cercare: ");
-        String parolaDaCercare = scanner.nextLine();
+        System.out.print("Inserisci la lettera iniziale: ");
+        char startingLetter = scanner.next().charAt(0);
+        System.out.print("Inserisci il nome del file da cercare: ");
+        String fileToSearch = scanner.next().toLowerCase();
         scanner.close();
 
-        boolean isParolaTrovata = fileSearchStrategy.search(parolaDaCercare, files);
+        // Aggiungi l'estensione .txt se non è già presente
+        if (!fileToSearch.endsWith(".txt")) {
+            fileToSearch += ".txt";
+        }
 
-        if (isParolaTrovata) {
-            System.out.println("Parola trovata: " + parolaDaCercare);
+        // Percorso della cartella "dizionario"
+        String basePath = "nomi_cose_città/src/dizionario/";
+        
+        // Percorso completo del file da cercare
+        String fullFilePath = Paths.get(basePath, fileToSearch).toString();
+
+        // Esempio di utilizzo:
+        String foundWord = fileSearchStrategy.search(startingLetter, fullFilePath);
+
+        if (foundWord != null) {
+            System.out.println("Parola trovata: " + foundWord);
         } else {
-            System.out.println("Parola non trovata: " + parolaDaCercare);
+            System.out.println("Nessuna parola trovata con la lettera " + startingLetter + " nel file " + fileToSearch);
         }
     }
 }
